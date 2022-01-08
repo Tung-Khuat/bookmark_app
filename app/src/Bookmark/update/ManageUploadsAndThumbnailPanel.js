0d7a380171a7d4d2159398f8bb03e2e79226579c
Tung Khuat <tung@vii.lu>
2022-01-08 18:54:55 +0200

    added upload delete functionailty

--- app/src/Bookmark/update/ManageUploadsAndThumbnailPanel.js
+++ app/src/Bookmark/update/ManageUploadsAndThumbnailPanel.js
@@ -3,6 +3,12 @@ import { Badge, IconButton } from '@mui/material'
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import AddUploadDialog from './AddUploadDialog'
+import { storage } from '../../state/store'
+import { ref, deleteObject } from "firebase/storage";
+import { useSnackbar } from 'notistack'
+import { connect } from 'react-redux'
+import { bindActionCreators, compose } from 'redux'
+import * as bookmarkActions from '../../state/actions/bookmarkActions'
 
 const PreviewsContainer = styled.div`
 	display: grid;
@@ -63,9 +69,11 @@ const AddUploadButton = styled.div`
 	}
 `
 
-export function ManageUploadsAndThumbnailPanel ({uploadLinks, bookmarkThumbnail, _updateUploadLinks, _setThumbnail, bookmarkUUID}) {
+function ManageUploadsAndThumbnailPanel ({uploadLinks, bookmarkThumbnail, _updateBookmark, _setThumbnail, bookmarkUUID}) {
 	const [currentSpotlight, setCurrentSpotlight] = useState(bookmarkThumbnail || uploadLinks[0])
 	const [addUploadDialogVisible, setAddUploadDialogVisible] = useState(false)
+	const { enqueueSnackbar } = useSnackbar();
+
 	const renderPreviews = (preview) => {
 		const { url } = preview
 		const isBookmarkThumbnail = url === bookmarkThumbnail.url
@@ -83,8 +91,32 @@ export function ManageUploadsAndThumbnailPanel ({uploadLinks, bookmarkThumbnail,
 		)
 	}
 
-	const handleUploadDelete = () => {
-		console.log("soon")
+	const handleUploadDelete = (uploadFile) => {
+		const confirmPrompt = window.confirm('Are you sure you want to delete this?')
+		if(!confirmPrompt)
+			return false
+		
+		const uploadFileRef = ref(storage, `bookmark-uploads/${bookmarkUUID}/${uploadFile.name}`)
+		const spotlightIndex = uploadLinks.findIndex(u => u.url === currentSpotlight.url)
+		const nextUploadInPreview = uploadLinks[spotlightIndex + 1 < uploadLinks.length ? spotlightIndex +1 : 0]
+		deleteObject(uploadFileRef).then(async () => {
+			enqueueSnackbar(`Successfully deleted ${uploadFile.name} file`, {variant: 'success'})
+			const updateUploadResponse = await _updateBookmark({
+				uploads: uploadLinks.filter(upload => upload.url !== currentSpotlight.url),
+				thumbnail: currentSpotlight.url === bookmarkThumbnail.url && nextUploadInPreview
+			}, bookmarkUUID)
+			if (updateUploadResponse) {
+				enqueueSnackbar('Updated bookmark uploads successful', {variant: 'success'})
+				setCurrentSpotlight(nextUploadInPreview)
+			} else {
+				enqueueSnackbar('Update book uploads failed. Please try again later.', {variant: 'error'})
+			}
+
+		}).catch((error) => {
+			console.log(error)
+			enqueueSnackbar('Failed to delete file.', {variant: 'error'})
+		});
+
 	}
 
 	return (
@@ -99,7 +131,7 @@ export function ManageUploadsAndThumbnailPanel ({uploadLinks, bookmarkThumbnail,
 					>
 						<Photo/>
 					</ActionButton> 
-					<ActionButton  onClick={handleUploadDelete}>
+					<ActionButton  onClick={()=>handleUploadDelete(currentSpotlight)}>
 						<Delete/>
 					</ActionButton> 
 				</SpotlightActions>
@@ -117,3 +149,14 @@ export function ManageUploadsAndThumbnailPanel ({uploadLinks, bookmarkThumbnail,
 	)
 }
 
+const mapState = ({
+}) => ({
+})
+
+const mapDispatchToProps = (dispatch) => ({
+	_updateBookmark: bindActionCreators(bookmarkActions._updateBookmark, dispatch),
+})
+
+export default compose(
+	connect(mapState, mapDispatchToProps)
+)(ManageUploadsAndThumbnailPanel)
