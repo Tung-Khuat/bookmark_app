1599b3cb063a02b22f6759b6c9e725068089e35f
Tung Khuat <tung@vii.lu>
2022-01-11 14:26:57 +0200

    moved upload operations to bookmark actions

--- app/src/state/actions/bookmarkActions.js
+++ app/src/state/actions/bookmarkActions.js
@@ -1,9 +1,16 @@
-import 'firebase/app';
 import 'firebase/functions';
-import { deleteObject, getDownloadURL, getMetadata, listAll, ref } from 'firebase/storage'
+import { deleteObject, getDownloadURL, getMetadata, listAll, ref, uploadBytesResumable } from 'firebase/storage'
+import { renameFileWithSanitizedName } from '../../utils/sanitizeFileName'
 import { firebaseApp, storage } from '../store'
 
 const functions = firebaseApp.functions('europe-west2')
+const bookmarkUploadBucket = 'bookmark-uploads'
+
+const getSanitizedNameFile = (file) => {
+	const sanitizedFile = renameFileWithSanitizedName(file)
+	const newFileName = sanitizedFile.name.replace(/ /g,"_")
+	return new File([file], newFileName);
+}
 
 export const _createBookmark = ({ title, description, link, thumbnail, uploads, tags, folder }) =>
 	async function (dispatch, getState, getFirebase) {
@@ -35,7 +42,7 @@ export const _updateBookmark = ({ title, description, link, thumbnail, tags, fol
 				'bookmarkUpdate',
 			)
 
-			const bookmarkFileListRef = ref(storage, `bookmark-uploads/${uuid}`)
+			const bookmarkFileListRef = ref(storage, `${bookmarkUploadBucket}/${uuid}`)
 			const fileList = await listAll(bookmarkFileListRef)
 			const uploads = []
 			for(let fileRef of fileList.items) {
@@ -73,7 +80,7 @@ export const _deleteBookmark = (uuid) =>
 			const deleteBookmark = functions.httpsCallable(
 				'bookmarkDelete',
 			)
-			const folderRef = ref(storage, `bookmark-uploads/${uuid}`)
+			const folderRef = ref(storage, `${bookmarkUploadBucket}/${uuid}`)
 			const fileList = await listAll(folderRef)
 			const promises = []
 			for(let item of fileList.items) {
@@ -98,3 +105,89 @@ export const _deleteBookmark = (uuid) =>
 			return false
 		}
 	}
+
+export const _upload = ({bookmarkUUID, file}) =>
+	async function (dispatch, getState, getFirebase) {
+		if (!bookmarkUUID || !file) {
+			return false
+		}
+
+		try {
+			const storageRef = ref(storage, `bookmark-uploads/${bookmarkUUID}/${file.name}`)
+			// Create the file metadata
+			const metadata = {
+				customMetadata: {
+					bookmarkUUID,
+				},
+			}
+
+			const operation = new Promise((resolve, reject) => {
+				const renamedFile = getSanitizedNameFile(file)
+				const uploadTask = uploadBytesResumable(storageRef, renamedFile, metadata)
+
+				const progressFunc = (snapshot) => {
+					// const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+					// console.log('Upload is ' + progress + '% done')
+					switch (snapshot.state) {
+						case 'paused':
+							console.log('Upload is paused')
+							break
+						case 'running':
+							console.log('Upload is running')
+							break
+						default:
+							break
+					}
+				}
+				const errorHandlingFunc = (error) => {
+					// A full list of error codes is available at
+					// https://firebase.google.com/docs/storage/web/handle-errors
+					switch (error.code) {
+						case 'storage/unauthorized':
+							// User doesn't have permission to access the object
+							console.error('storage permission denied')
+							reject(error.code)
+							break
+
+						case 'storage/canceled':
+							// User canceled the upload
+							console.error('upload cancelled')
+							reject(error.code)
+							break
+
+						case 'storage/unknown':
+							// Unknown error occurred, inspect error.serverResponse
+							console.error('unknown storage error')
+							reject(error.code)
+							break
+
+						default:
+							console.error('default storage error')
+							break
+					}
+				}
+
+				const completeFunc = () => {
+					// Upload completed successfully
+					resolve(uploadTask)
+					console.log(`%c Successfully uploaded file`, 'color: green')
+				}
+
+				// Listen for state changes, errors, and completion of the upload.
+				uploadTask.on(
+					"state_changed",
+					progressFunc,
+					errorHandlingFunc,
+					completeFunc,
+				)
+			})
+			const result = await operation
+			return result
+		} catch (ex) {
+			console.log(ex)
+			if (window.sentry) window.sentry.captureException(ex)
+			return false
+		}
+	}
+
+	
