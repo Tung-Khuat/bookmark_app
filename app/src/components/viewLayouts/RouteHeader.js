05380e532a8cdc58e23af69d1971942665690d70
Tung Khuat <tung@vii.lu>
2022-01-12 15:59:37 +0200

    added way to logout through user profile dialog

---
+++ app/src/components/viewLayouts/RouteHeader.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import { Subtext } from '../styledComponents/BasicComponents'
+
+const Container = styled.div`
+	width: 100%;
+	padding: 8px 0;
+`
+const HeaderSection = styled.div`
+	margin: 0 32px;
+	div {
+		font-weight: 400;
+    	font-size: 1.5em;
+	}
+`
+
+export default function RouteHeader ({ header,  subheader }) {
+
+	return (
+		<Container>
+			<HeaderSection>
+				<div>{header}</div>
+				<Subtext>{subheader}</Subtext>
+			</HeaderSection>
+		</Container>
+	)
+}
