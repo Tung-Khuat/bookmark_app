e30efc7156180f3c1ea327017b5dd23772e6a788
Tung Khuat <tung@vii.lu>
2022-03-30 11:07:31 +0300

    reversed contrast colors

--- app/src/themes/defaultThemes.js
+++ app/src/themes/defaultThemes.js
@@ -1,9 +1,10 @@
 const defaultThemeColors = {
-    primaryContrastA: "#ffffff",
-    primaryContrastB: "#212121",
-    secondaryContrastA: "#e2e2e2",
-    secondaryContrastB: "#81171b",
-    highlight: "#b72f33",
+    primaryContrastB: "#e9e9e9",
+    primaryContrastA: "#2d3142",
+    secondaryContrastB: "#bfc0c0",
+    secondaryContrastA: "#4f5d75",
+    neutral: "#55828b",
+    highlight: "#ef8354",
 }
 
 const includeAllProperties = (themeColorsProps) => {
@@ -12,7 +13,7 @@ const includeAllProperties = (themeColorsProps) => {
         ...themeColorsProps,
     }
     if (!theme.paletteArray) {
-        theme.paletteArray = [ theme.primaryContrastB, theme.secondaryContrastB, theme.neutral, theme.secondaryContrastA, theme.primaryContrastA, theme.highlight ]
+        theme.paletteArray = [ theme.primaryContrastA, theme.secondaryContrastA, theme.neutral, theme.secondaryContrastB, theme.primaryContrastB, theme.highlight ]
     }
     return theme
 }
@@ -21,61 +22,67 @@ export const defaultThemes = [
     {
         name: 'Default Theme 1',
         themeColors: includeAllProperties({
-            primaryContrastA: "#ffffff",
-            primaryContrastB: "#2d3142",
-            secondaryContrastA: "#bfc0c0",
-            secondaryContrastB: "#4f5d75",
+            primaryContrastA: "#2d3142",
+            primaryContrastB: "#e9e9e9",
+            secondaryContrastA: "#4f5d75",
+            secondaryContrastB: "#bfc0c0",
             neutral: "#55828b",
             highlight: "#ef8354",
-            // paletteArray: [ "#81171b", "#b72f33", "#e2e2e2", "#ffffff", "#212121"],
         }),
     },
     {
         name: 'Default Theme 2',
         themeColors: includeAllProperties({
-            primaryContrastA: "#ffffff",
-            primaryContrastB: "#353535",
-            secondaryContrastA: "#d9d9d9",
-            secondaryContrastB: "#284b63",
+            primaryContrastA: "#353535",
+            primaryContrastB: "#e9e9e9",
+            secondaryContrastA: "#284b63",
+            secondaryContrastB: "#d9d9d9",
             neutral: "#3c6e71",
             highlight: "#ccdbfd",
-            // paletteArray: ["#353535","#3c6e71","#ffffff","#d9d9d9", "#284b63"],
         }),
     },
     {
         name: 'Default Theme 3',
         themeColors: includeAllProperties({
-            primaryContrastA: "#e0fbfc",
-            primaryContrastB: "#293241",
-            secondaryContrastA: "#98c1d9",
-            secondaryContrastB: "#3d5a80",
+            primaryContrastA: "#293241",
+            primaryContrastB: "#e0fbfc",
+            secondaryContrastA: "#3d5a80",
+            secondaryContrastB: "#98c1d9",
             neutral: "#2a6f97",
             highlight: "#ee6c4d",
-            // paletteArray: ["#3d5a80","#98c1d9","#e0fbfc","#ee6c4d", "#293241"],
         }),
     },
     {
         name: 'Default Theme 4',
         themeColors: includeAllProperties({
-            primaryContrastA: "#f1faee",
-            primaryContrastB: "#1d3557",
-            secondaryContrastA: "#a8dadc",
-            secondaryContrastB: "#22577a",
+            primaryContrastA: "#1d3557",
+            primaryContrastB: "#f1faee",
+            secondaryContrastA: "#22577a",
+            secondaryContrastB: "#a8dadc",
             neutral: "#457b9d",
             highlight: "#e63946",
-            // paletteArray: ["#e63946","#f1faee","#a8dadc","#457b9d", "#1d3557"],
         }),
     },
     {
         name: 'Default Theme 5',
         themeColors: includeAllProperties({
-            primaryContrastA: "#cad2c5",
-            primaryContrastB: "#2f3e46",
-            secondaryContrastA: "#84a98c",
-            secondaryContrastB: "#354f52",
+            primaryContrastA: "#2f3e46",
+            primaryContrastB: "#cad2c5",
+            secondaryContrastA: "#354f52",
+            secondaryContrastB: "#84a98c",
             neutral: "#52796f",
             highlight: "#f4d35e",
-            // paletteArray: ["#cad2c5","#84a98c","#52796f","#354f52", "#2f3e46"],
+        }),
+    },
+    {
+        name: 'Default Theme 6',
+        themeColors: includeAllProperties({
+            primaryContrastA: "#212529",
+            primaryContrastB: "#F8F9FA",
+            secondaryContrastA: "#343A40",
+            secondaryContrastB: "#E9ECEF",
+            neutral: "#6C757D",
+            highlight: "#2ca6a4",
         }),
     },
 ]
